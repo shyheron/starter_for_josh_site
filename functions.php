629d43722b7dc5731411af172930d959e458ea74
asmasaeed <asma.saeedh@gmail.com>
2022-04-15 20:37:55 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -40,3 +40,17 @@ function footer_left() {
 }
 add_action( 'widgets_init', 'footer_left' );
 ?>
+<?php
+function footer_right() {
+
+  register_sidebar( array(
+    'name'          => 'footer-right',
+    'id'            => 'footer-r',
+    'before_widget' => '<div>',
+    'after_widget'  => '</div>',
+  
+  ) );
+
+}
+add_action( 'widgets_init', 'footer_right' );
+?>
