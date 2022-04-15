ba4d9e50b32d79f5d3458b73e211cd250b715c77
asmasaeed <asma.saeedh@gmail.com>
2022-04-15 20:37:57 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,11 +4,14 @@
             <div class="row">
                 <div class="col-md-6">
                 <!--    <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a> -->
-                </div>
-                <div class="col-md-6 widgetleft">
                 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>    
+            </div>
+                <div class="col-md-6 widgetleft">
+                
+                    <!-- <p>Terms and Conditions</p> -->
+                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
 endif; ?>
-                    <p>Terms and Conditions</p>
                 </div>
 
             </div> <!-- row -->
