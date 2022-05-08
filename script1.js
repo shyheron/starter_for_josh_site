82898798c2c29e13fc57867f7e07071808705b24
asmasaeed <asma.saeedh@gmail.com>
2022-05-08 16:42:47 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,7 +3,8 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.timeline().from('.bg1', {scale:0, duration:2})
+gsap.timeline()
+.from('.bg1', {scale:0, duration:2})
 .from('.navbar-brand', {opacity:0, scale: 0, ease:"back"})
 .from('.tag', {opacity:0, y: 10, ease:"back"});
 
