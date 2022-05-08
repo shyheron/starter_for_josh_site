7a02c512e03859567b42c5e606f622553106df50
asmasaeed <asma.saeedh@gmail.com>
2022-05-08 16:27:57 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -7,14 +7,25 @@ gsap.timeline().from('.bg1', {scale:0, duration:2})
 .from('.navbar-brand', {opacity:0, scale: 0, ease:"back"})
 .from('.tag', {opacity:0, y: 10, ease:"back"});
 
-gsap.from(".about-text", {
-    duration:2,
-    y:250,
-    opacity:0,
-    scrollTrigger:{
-       trigger:".container h2",
-    scrub:1, //4 seconds of smooting
-}
+const tl = gsap.timeline({
+    scrollTrigger: {
+     trigger: '.aboutbg',
+     markers:true,
+     start:"bottom 90%", 
+     end:"top 30%",
+     scrub:3, 
+          }
+  
+});
+
+tl.from('.about-text',{
+ duration:1,
+      y:250,
+ opacity:0,
+}).from('.blocker',{
+ duration:1,
+ x:250,
+opacity:0,
 });
 
     
